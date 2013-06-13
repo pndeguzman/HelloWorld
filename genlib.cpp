60c14e625d9a1c62748e24d190f3eac9b1097c03
pndeguzman <pndeguzman@gmail.com>
2013-06-13 20:06:07 +1000

    del1cs3171

---
+++ genlib.cpp
@@ -0,0 +1,26 @@
+/*
+ * File: genlib.cpp
+ * ----------------
+ * This file implements the genlib.h interface
+ */
+
+#include "genlib.h"
+#include <cstdlib>
+#include <iostream>
+
+ErrorException::ErrorException(string m="unspecified custom error") 
+: msg(m) {
+}
+
+ErrorException::~ErrorException() throw() {}
+
+const char* ErrorException::what() const throw() {
+    return this->msg.c_str(); 
+}
+
+void Error(string str) {
+    ErrorException err(str);
+    throw err;
+}
+
+
